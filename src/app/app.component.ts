33bcaa5e7b616d4cb2be4f01216b86e4343b2702
dmad1989 <dmad1989@gmail.com>
2021-02-16 22:10:18 +0300

    experiments_1 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'For Special Girl:';
+  text = 'Alina i love you';
+  show: Boolean = false;
+
+  onSelect(isshow: Boolean): void {
+    this.show = !isshow;
+  }
+
 }
